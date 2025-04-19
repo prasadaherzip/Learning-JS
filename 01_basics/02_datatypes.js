686fc48bfadfccb6a7bbb57440355387b7cd2de0
Prasad <aherprasad2811@gmail.com>
2025-04-19 10:37:01 +0000

    new

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,26 @@
 "use strict"; // treat all JS code as newer version
 
-alert("hello")
+// alert("hello")
+
+let name = "prasad"
+let age = 18
+let isLoggedIn = true
+let state = null
+let abcd
+
+/* primitive datatypes */
+
+// number => biggest val is 2 raised to 53
+// bigint used in social media websites
+// null = standalone value
+// undefined = no value specified
+// string = ""
+// boolean = true/false
+// symbol = uniqueness
+
+console.table([name, age, isLoggedIn, state, abcd])
+
+/* objects */
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);       //null is an object
