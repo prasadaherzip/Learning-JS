53e8e3536fef0cb337ba0570748de392e4ebdcd5
Prasad <aherprasad2811@gmail.com>
2025-04-19 07:59:20 +0000

    variables update

---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "aherprasad@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" //do not use this declaration
+
+let accountState;
+
+// accountId = 2 //
+
+console.log(accountId);
+
+console.table([accountPassword, accountCity, accountId])
+
+/*
+perfer not to use var
+due to issue in block and functional scope
+*/
