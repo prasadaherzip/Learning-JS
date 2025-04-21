ba86b3eb9f86fe8eb7b182af0ee3791a4ae19278
Prasad <aherprasad2811@gmail.com>
2025-04-21 13:32:05 +0000

    new lec

--- 01_basics/04_operations.js
+++ 01_basics/04_operations.js
@@ -6,6 +6,8 @@ console.log(3+3+4+"5")  //prints out 105
 
 console.log(3+3+4+"5"+3+5+5) //print 105355
 
-/* adds before string a string is encountered,
-later treat every successive number as a string */
+/* adds mathamatically before  a string is encountered,
+later treats every successive number as a string */
 
+console.log(+true)
+console.log(+"")
---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,9 @@
+
+console.log(null>1)
+console.log(null==0)
+console.log(null>=0)
+
+console.log(null===null)
+console.log(undefined===null)   // === comparison of data types
+
+console.log("2"===2)            //string and number 
