226c9480a802b6c131236f24f9129dd2c6e72670
Prasad <aherprasad2811@gmail.com>
2025-04-27 14:37:16 +0000

    accessing objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,50 +1,43 @@
-const jsUser={
-    name:"Prasad",
-    age:18,
-    location:"Mumbai",
-    email:"prasad@google.com",
-    isLoggedIn:false,
-    lastLoginDays:["Monday", "Tuesday"],
+const mySym = Symbol("key1");
 
-}
+const jsUser = {
+    name: "Prasad",
+    "fullname": "Prasad Aher",       // when the key is a "string": value
+    age: 18,
+    [mySym]: "myKey1",
+    location: "Mumbai",
+    email: "prasad@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Tuesday"],
+};
 
-console.log("")
+/* Access elements of an object */
 
-/* access elements of an object*/
-
-console.log(jsUser.email)           //object.key, dot notation
-console.log(jsUser["email"])        //object["key"], square notation
-
-const JsUser2={
-    "fullname":"Prasad Aher"        //when the key is a "string":value
-}
-
-console.log(JsUser2["fullname"])
+console.log(jsUser.email);           // object.key, dot notation
+console.log(jsUser["email"]);        // object["key"], square bracket notation
 
 console.log("")
 
-/* Using a symbol data type as a key*/
+console.log(jsUser["fullname"]);     // Corrected missing closing parenthesis
 
-const mySym = Symbol("key1")
-const jsUser3 = {
-    mySym: "myKey1"
+/* Accessing a symbol key from an object */
+
+console.log(jsUser[mySym]);           
+/* manipulating values in an object */
+
+jsUser.email = "prasad@chatgpt.com";    // changing the value of a key
+// Object.freeze(jsUser);                  // Capital "O" needed in Object
+jsUser.email = "prasad@microsoft.com";  // won't change because object is frozen
+console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("Hello JS User");
 }
 
-console.log(jsUser3.mySym)
-console.log(typeof(jsUser3.mySym))      //symbol data types accessed as a string
-
-//revised
-const jsUser4 = {
-    [mySym]: "myKey1"
+jsUser.greeting2 = function(){
+    console.log(`Hello JS User, ${this.name}`);     //using string interpolation to call a key from an object
 }
 
-console.log(jsUser4[mySym])
-console.log(typeof(jsUser4[mySym]) 
 
-/* back to top */
-
-jsUser.email= "prasad@chatgpt.com"           //changing the values of a key in the objects
-object.freeze(jsUser)
-jsUser.email= "prasad@microsoft.com"
-console.log(jsUser)
-
+console.log(jsUser.greeting());
+console.log(jsUser.greeting2());
