47e01d76e97d0a5e814def5400dae9e10b752e03
Prasad <aherprasad2811@gmail.com>
2025-05-05 11:10:04 +0000

    new

---
+++ 03_basics/01_shoppingcart.js
@@ -0,0 +1,29 @@
+// function cartPrice(val1, val2,...num1){            // rest and spread operator
+//     return num1
+// }
+
+// console.log(cartPrice(200,300,400,2000))
+
+// const user - {
+//     username: "hitesh",
+//     price: 199
+// }
+
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is 
+//         ${anyobject.price}`);
+// }
+
+// handleObject({
+//     unsername :"sam",
+//     price:399
+// })
+
+const myNewArray = [2000,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+    
+}
+
+console.log(returnSecondValue)
