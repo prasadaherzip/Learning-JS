10729f0bbd1aaccb27b718a62dd7a7d94a8d0faf
Prasad <aherprasad2811@gmail.com>
2025-05-23 18:46:05 +0000

    new

--- state.js
+++ state.js
@@ -1,11 +1,8 @@
-const myNo = [1,2,3,4,5,6]
+const myNo = [1,2,3,4,5,6,[1,5,30,4,5,]]
 
 const myArr = [ "revive", "health", "save", "discard"]
 
 myNo.push(6,4,3,2,1)
 myArr.push("pubg","Settings",'test')
 
-console.log(myNo)
-console.log(myArr)
-
-console.log(myArr.indexOf (13))
+console.log(myNo(7))
